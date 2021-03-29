c02aff7c1484dc38b0695334d7dc6d6864e44a55
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2021-03-29 18:07:06 -0500

    cambios

--- Sources/Epayco/Epayco.swift
+++ Sources/Epayco/Epayco.swift
@@ -1,3 +1,14 @@
-struct Epayco {
-    var text = "Hello, World!"
+public struct Epayco {
+    let API_URL     = "https://api.secure.epayco.xyz"
+    let SECURE_URL  = "String"
+    var PUBLIC_KEY  : String
+    var PRIVATE_KEY : String
+    public init(publicKey: String, privateKey: String){
+        self.PUBLIC_KEY = publicKey
+        self.PRIVATE_KEY = privateKey
+    }
+    
+    public func prueba() -> String {
+        return self.PUBLIC_KEY
+    }
 }
