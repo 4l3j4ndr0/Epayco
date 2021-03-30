cc71c6078c9b6e031821ee49756ad6afd9003fdd
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2021-03-30 17:53:36 -0500

    cambios

--- Sources/Epayco/Models/CreditToken.swift
+++ Sources/Epayco/Models/CreditToken.swift
@@ -1,7 +1,7 @@
 
 public class CreditToken {
-    var card_number     : String {get}
-    var exp_year        : String {get}
-    var exp_month       : String {get}
-    var cvv             : String {get}
+    var card_number     : String
+    var exp_year        : String
+    var exp_month       : String
+    var cvv             : String 
 }
