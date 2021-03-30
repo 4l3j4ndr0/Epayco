35bd93b4c68f708a8658abaea2a12747a52ca6c0
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2021-03-30 18:31:12 -0500

    cambios

--- Sources/Epayco/Models/CreditToken.swift
+++ Sources/Epayco/Models/CreditToken.swift
@@ -1,11 +1,11 @@
 
-public class CreditToken {
-    public var card_number     : String {get}
-    public var exp_year        : String {get}
-    public var exp_month       : String {get}
-    public var cvv             : String {get}
+public class CreditToken : NSObject {
+    var card_number     : NSString
+    var exp_year        : NSString
+    var exp_month       : NSString
+    var cvv             : NSString
     
-    public init(card_number: String, exp_year: String, exp_month: String, cvv: String) {
+    public init(card_number: NSString, exp_year: NSString, exp_month: NSString, cvv: NSString) {
         self.card_number    = card_number
         self.exp_year       = exp_year
         self.exp_month      = exp_month
