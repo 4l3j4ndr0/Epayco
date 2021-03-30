ef526f91314c3d923061dba1bc67af12265f649b
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2021-03-30 18:25:59 -0500

    cambios

--- Sources/Epayco/Models/CreditToken.swift
+++ Sources/Epayco/Models/CreditToken.swift
@@ -1,9 +1,9 @@
 
 public class CreditToken {
-    var card_number     : String {get}
-    var exp_year        : String {get}
-    var exp_month       : String {get}
-    var cvv             : String {get}
+    public var card_number     : String {get}
+    public var exp_year        : String {get}
+    public var exp_month       : String {get}
+    public var cvv             : String {get}
     
     public init(card_number: String, exp_year: String, exp_month: String, cvv: String) {
         self.card_number    = card_number
